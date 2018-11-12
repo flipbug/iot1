6ada307af2819202d2ea865c36e3dc199bf2d39b
David Pacassi Torrico <dpacassi@gmail.com>
2018-11-12 20:31:17 +0100

    Enable/disable the LED according to the rotary angle's input

--- p05/control_loop_template.py
+++ p05/control_loop_template.py
@@ -15,6 +15,15 @@ LESHAN_SERVER_PORT = "8080"
 CLIENT_1_NAME = "iot-pi-right-front"
 CLIENT_2_NAME = "iot-pi-left-front"
 
+ROTARY_CLIENT = CLIENT_1_NAME
+LED_CLIENT = CLIENT_1_NAME
+LED_INSTANCE = "5550"
+
+# Notes Pacassi:
+# This code turns on a LED on LED_CLIENT as soon as
+# ROTARY_CLIENT's rotary sensor exceeds 50% (>= 512).
+
+
 # Add additional IoT client names here if needed
 # ################################################
 
@@ -26,9 +35,8 @@ BASE_URL = "http://" + LESHAN_SERVER_IP + ":" + LESHAN_SERVER_PORT + \
 SLEEP_TIME = float(0.7)  # unit is seconds
 
 # Example URL concatenations ...
-URL_ROTARY_ANGLE_SENSOR = BASE_URL + "/" + CLIENT_1_NAME + "/3202/0/5600"
-
-URL_LED = BASE_URL + "/" + CLIENT_2_NAME + "/3201/0/5550"
+URL_ROTARY_ANGLE_SENSOR = BASE_URL + "/" + ROTARY_CLIENT + "/3202/0/5600"
+URL_LED = BASE_URL + "/" + LED_CLIENT + "/3201/0/5550"
 
 # Add additional resources here if needed
 # #############################################
@@ -70,20 +78,20 @@ def main():
   signal.signal(signal.SIGINT, signal_handler)
 
   # Find Resource example ...
-  print("Get source tree \n")
-  print(BASE_URL)
-  source_tree = example_get(BASE_URL)
-  print(json.dumps(source_tree, indent=4, sort_keys=True))
-  print("\n")
-  source_tree_client = example_get(BASE_URL + "/" + CLIENT_1_NAME)
-  print(json.dumps(source_tree_client, indent=4, sort_keys=True))
-  print("\n")
-  source_tree_ressource = example_get(BASE_URL + "/" + CLIENT_1_NAME + "/3202")
-  print(json.dumps(source_tree_ressource, indent=4, sort_keys=True))
-  print("\n")
-  source_tree_ressource = example_get(BASE_URL + "/" + CLIENT_1_NAME + "/3201")
-  print(json.dumps(source_tree_ressource, indent=4, sort_keys=True))
-  print("\n")
+  #print("Get source tree \n")
+  #print(BASE_URL)
+  #source_tree = example_get(BASE_URL)
+  #print(json.dumps(source_tree, indent=4, sort_keys=True))
+  #print("\n")
+  #source_tree_client = example_get(BASE_URL + "/" + CLIENT_1_NAME)
+  #print(json.dumps(source_tree_client, indent=4, sort_keys=True))
+  #print("\n")
+  #source_tree_ressource = example_get(BASE_URL + "/" + CLIENT_1_NAME + "/3202")
+  #print(json.dumps(source_tree_ressource, indent=4, sort_keys=True))
+  #print("\n")
+  #source_tree_ressource = example_get(BASE_URL + "/" + CLIENT_1_NAME + "/3201")
+  #print(json.dumps(source_tree_ressource, indent=4, sort_keys=True))
+  #print("\n")
   # ##########################################################################
 
   print("Entering Control Loop")
@@ -97,6 +105,12 @@ def main():
     # Write your code here. Values can be sent
     # towards actuators with the example_put function
     # #############################################
+    led_value = "False"
+
+    if sensor_value >= 512:
+      led_value = "True"
+
+    example_put(led_value, URL_LED, LED_INSTANCE)
 
     # #############################################
 
