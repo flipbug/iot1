9cc72047f5976b0f4f35d1336051a3b47d641feb
Savino Jossi <savino.jossi@gmail.com>
2018-12-03 20:14:18 +0100

    motion is sensed and message sent to broker

--- project/motionsensor/motionsensor.py
+++ project/motionsensor/motionsensor.py
@@ -1,6 +1,23 @@
 import os
 from paho.mqtt.publish import single
+import grovepi
 
 host = os.environ['MQTT_BROKER_IP']
 
-single('megasec/motionsensor', payload='motion detected', hostname=host)
+
+# PIR sensor setup
+pir = 2
+grovepi.pinMode(pir, "INPUT")
+armed = 1
+
+while(True):
+    try:
+        if (grovepi.digitalRead(pir) == 1 and armed == 1):
+            print("Motion detected. Sensor disarmed")
+            single('megasec/motionsensor', payload='motion detected', hostname=host)
+            armed = 0
+        if (grovepi.digitalRead(pir) == 0):
+            print("No more motion. Sensor armed again")
+            armed = 1
+    except IOError:
+        print("There was an I/O Error :(")
