c15734bb933960a13ceae970bce1fe38ab3c4a45
Savino Jossi <savino.jossi@gmail.com>
2018-12-03 19:46:22 +0100

    improve sync-script with helpful hints

--- project/sync.sh
+++ project/sync.sh
@@ -2,6 +2,7 @@
 if [ -z $DEVPI ]; then 
   echo -n "Please enter Rasperry Pi IP: "
   read DEVPI;
+  echo "set \`export DEVPI=<IP-Address>\` for persistent magic"
 fi
 
 rsync -avhz --update . $DEVPI:/home/pi/megasec/
