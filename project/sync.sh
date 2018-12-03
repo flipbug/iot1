59b1a1d6dbb3a3d6755d0879f83eeeb96d45d896
Savino Jossi <savino.jossi@gmail.com>
2018-12-03 20:19:44 +0100

    add command line arguments to sync.sh

--- project/sync.sh
+++ project/sync.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 if [ -z $DEVPI ]; then 
-  echo -n "Please enter Rasperry Pi IP: "
-  read DEVPI;
-  echo "set \`export DEVPI=<IP-Address>\` for persistent magic"
+  if [ -z $1 ]; then
+    echo -n "Please enter Rasperry Pi IP: "
+    read DEVPI;
+    echo "set \`export DEVPI=<IP-Address>\` for persistent magic or run as ${0} <ip address>"
+  else
+    DEVPI=$1
+  fi
 fi
 
 rsync -avhz --update . $DEVPI:/home/pi/megasec/
