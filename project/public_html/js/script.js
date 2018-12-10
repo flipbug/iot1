53837b5424d1c269c8d3b04b5f204a96dbb8caee
David Pacassi Torrico <dpacassi@gmail.com>
2018-12-10 20:35:20 +0100

    Add an image lightbox

--- project/public_html/js/script.js
+++ project/public_html/js/script.js
@@ -1,21 +1,28 @@
 $(document).ready(function($) {
   var $img = $('#image');
   var $imgTitle = $('#image-title');
+  var $imgLink = $('#image-link');
+
+  $imgLink.magnificPopup({ type: 'image' });
 
   $('#snapshot-link, #capture-link').click(function(event) {
     event.preventDefault();
     var id = $(this).attr('id');
     var newSrc = '';
+    var newLink = '';
 
     if (id === 'snapshot-link') {
       $imgTitle.text('Snapshot image');
       newSrc = $img.attr('src').replace('capture.jpg', 'snapshot.png');
+      newLink = $imgLink.attr('href').replace('capture.jpg', 'snapshot.png');
     } else {
       $imgTitle.text('Capture image');
       newSrc = $img.attr('src').replace('snapshot.png', 'capture.jpg');
+      newLink = $imgLink.attr('href').replace('snapshot.png', 'capture.jpg');
     }
 
     $img.attr('src', newSrc);
+    $imgLink.attr('href', newLink);
 
     return false;
   })
